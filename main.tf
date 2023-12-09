4487d02d7c37ff32ff431bf5e9bd02d1d88e4bea
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2023-12-09 18:51:52 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,35 +1,32 @@
+resource "vultr_ssh_key" "enre" {
+  name = "enre"
+  ssh_key = "SSH_PUBLIC_KEY_REPLACEME"
+}
+
 resource "vultr_instance" "west-main" {
     plan = "vc2-1c-1gb"
     region = "sea"
     os_id = 1743
+    ssh_key_ids = ["${vultr_ssh_key.enre.id}"]
 }
 
 resource "vultr_instance" "west-second" {
     plan = "vc2-1c-1gb"
     region = "sea"
     os_id = 1743
-}
-
-resource "vultr_instance" "west-third" {
-    plan = "vc2-1c-1gb"
-    region = "sea"
-    os_id = 1743
+    ssh_key_ids = ["${vultr_ssh_key.enre.id}"]
 }
 
 resource "vultr_instance" "east-main" {
     plan = "vc2-1c-1gb"
     region = "ewr"
     os_id = 1743
+    ssh_key_ids = ["${vultr_ssh_key.enre.id}"]
 }
 
 resource "vultr_instance" "east-second" {
     plan = "vc2-1c-1gb"
     region = "ewr"
     os_id = 1743
-}
-
-resource "vultr_instance" "east-third" {
-    plan = "vc2-1c-1gb"
-    region = "ewr"
-    os_id = 1743
+    ssh_key_ids = ["${vultr_ssh_key.enre.id}"]
 }
