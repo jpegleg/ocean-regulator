da2189e0949eb63a5730078df6b47cf667060647
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2024-03-02 21:45:38 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -8,12 +8,7 @@ output "west-second_ip_addr" {
   description = "West second node IP address."
 }
 
-output "west_main_ip_addr" {
-  value       = vultr_instance.east-main.main_ip
-  description = "West coast initial control node IP."
-}
-
-output "west-second_ip_addr" {
-  value       = vultr_instance.east-second.main_ip
-  description = "West second node IP address."
+output "west-third_ip_addr" {
+  value       = vultr_instance.west-third.main_ip
+  description = "West coast third node IP."
 }
